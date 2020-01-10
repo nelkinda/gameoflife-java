a768efc24884248d5e2802de0ec08af4c58c12cd
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-01-10 20:34:34 +0530

    Change Cucumber to strict mode, fixes warning when running Cucumber through Maven+Junit.

--- src/test/java/com/nelkinda/training/gameoflife/RunCukesTest.java
+++ src/test/java/com/nelkinda/training/gameoflife/RunCukesTest.java
@@ -4,7 +4,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@CucumberOptions(features="src/test/resources/features")
+@CucumberOptions(
+        features="src/test/resources/features",
+        strict = true
+)
 @RunWith(Cucumber.class)
 public class RunCukesTest {
 }
