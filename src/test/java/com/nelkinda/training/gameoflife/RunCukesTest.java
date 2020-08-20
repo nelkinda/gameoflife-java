d1bc191bbf928d28009959af1487c1e3d6b20029
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-08-20 18:02:58 +0530

    Update Cucumber to version 6.5.0.

--- src/test/java/com/nelkinda/training/gameoflife/RunCukesTest.java
+++ src/test/java/com/nelkinda/training/gameoflife/RunCukesTest.java
@@ -1,13 +1,12 @@
 package com.nelkinda.training.gameoflife;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
-        strict = true
+        features = "src/test/resources/features"
 )
-@RunWith(Cucumber.class)
+@RunWith(io.cucumber.junit.Cucumber.class)
+@io.cucumber.junit.platform.engine.Cucumber
 public class RunCukesTest {
 }
