360583a43266d59d6a4f0771a515f44937c5654d
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-10-04 18:26:03 +0530

    Fix issues with Maven build/test.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,6 @@ module nelkinda.training.gameoflife {
     requires lombok;
     exports com.nelkinda.training.gameoflife;
     opens com.nelkinda.training.gameoflife;
+    opens com.nelkinda.training.gameoflife.life2d;
+    opens com.nelkinda.training.gameoflife.life3d;
 }
